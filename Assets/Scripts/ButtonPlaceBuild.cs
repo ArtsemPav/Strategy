f654fb7b1d620ced1001de0cae36dc89799ae1c4
Artsem Pauliushchyk <artsiom.p@customgamesstudio.com>
2026-01-06 21:23:47 +0300

    Add buildings

---
+++ Assets/Scripts/ButtonPlaceBuild.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPlaceBuild : MonoBehaviour
+{
+    public GameObject building;
+    public void PlaceBuilding()
+    {
+        Instantiate(building, Vector3.zero, Quaternion.identity);
+    }
+}
